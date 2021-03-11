7e88005ad7d3872841bd6345d785a020ae3bb962
Welsey  Neri Bernadoque <admtelemetria@AUTOSENDER.AUTOSENDER.COM.BR>
2021-03-11 17:11:22 -0300

    scroll suave implementado nativo scrollIntoView

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -4,18 +4,12 @@ export default function scroll() {
   function scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
+
     const section = document.querySelector(href);
     section.scrollIntoView({
       behavior: 'smooth',
       block: 'start',
     });
-
-    // const topo = section.offsetTop;
-    // console.log(topo);
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
   }
   linksInternos.forEach((link) => {
     link.addEventListener('click', scrollToSection);
