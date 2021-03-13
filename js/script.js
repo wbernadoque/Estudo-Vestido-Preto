21f8de4e73241420c9e5d446da2a0649b05c5699
wbernadoque <wnbeq@hotmail.com>
2021-03-12 22:06:47 -0300

    Cards criados a partir de objeto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 import galeriaDeFotos from './modules/galeria-de-fotos.js';
 import botoesSelecao from './modules/botoes.js';
 import scroll from './modules/scroll.js';
-
+import cards from './modules/cards.js';
 galeriaDeFotos();
 botoesSelecao();
 scroll();
+cards();
