d9f7e2b292c4b0ed4cbccafc27643bc3f163a103
Welsey  Neri Bernadoque <admtelemetria@AUTOSENDER.AUTOSENDER.COM.BR>
2021-03-15 17:07:54 -0300

    customização feita (faltando refatorar)

--- js/modules/botoes.js
+++ js/modules/botoes.js
@@ -1,5 +1,5 @@
 export default function botoesSelecao() {
-  const lista = document.querySelector('.botao');
+  const botoes = document.querySelector('.botao');
   const customizar = document.querySelector('.customizar');
   const vestido = {
     decote: '',
@@ -37,9 +37,10 @@ export default function botoesSelecao() {
     },
     {
       imagem: 'img/image 1.png',
-      nome: 'Manga curta',
+      nome: 'Manga Curta',
     },
   ];
+
   const botoesComprimento = [
     {
       imagem: 'img/image 1.png',
@@ -52,31 +53,142 @@ export default function botoesSelecao() {
   ];
   const tamanho = ['p', 'm', 'g', 'gg'];
 
+  const tamanhoReverso = tamanho.reverse();
+  const comprimentoReverso = botoesComprimento.reverse();
   const decoteReverso = botoesDecote.reverse();
-  customizar.querySelectorAll('a').forEach((item, index) => {
-    if (item.classList.contains('ativo') && index === 0) {
-      decoteReverso.forEach((item) => {
-        const botao = document.createElement('a');
-        const imagem = document.createElement('img');
-        const primeiroFilho = lista.firstChild;
-        lista.insertBefore(botao, primeiroFilho);
-        imagem.src = item.imagem;
-        botao.appendChild(imagem);
-        botao.appendChild(document.createTextNode(item.nome));
-      });
-    } else if (!item.classList.contains('ativo') && index === 0) {
-    }
-  });
+  const mangaReverso = botoesManga.reverse();
+  const botoesCustomizarNode = customizar.querySelectorAll('a');
+  const botoesCustomizar = Array.from(botoesCustomizarNode);
 
-  lista.querySelectorAll('a').forEach((item, index) => {
-    item.addEventListener('click', () => {
-      if (item.classList.contains('ativo')) {
-        item.classList.remove('ativo');
-      } else {
-        item.classList.add('ativo');
-        vestido.decote = item.childNodes[1].nodeValue;
-        console.log(vestido);
+  function verificarAtivo() {
+    const ativo = document.querySelectorAll('.customizar a');
+    while (botoes.firstChild) {
+      // remove os botoes que não estao ativos
+
+      botoes.removeChild(botoes.firstChild);
+    }
+    ativo.forEach((item, index) => {
+      if (index === 0 && item.classList.contains('ativo')) {
+        decoteReverso.forEach((item) => {
+          const botao = document.createElement('a');
+          const imagem = document.createElement('img');
+          const primeiroFilho = botoes.firstChild;
+          botoes.insertBefore(botao, primeiroFilho);
+          imagem.src = item.imagem;
+          botao.appendChild(imagem);
+          botao.appendChild(document.createTextNode(item.nome));
+          if (item.nome == vestido.decote) {
+            botao.classList.add('ativo');
+          }
+        });
+      } else if (index === 1 && item.classList.contains('ativo')) {
+        mangaReverso.forEach((item) => {
+          const botao = document.createElement('a');
+          const imagem = document.createElement('img');
+          const primeiroFilho = botoes.firstChild;
+          botoes.insertBefore(botao, primeiroFilho);
+          imagem.src = item.imagem;
+          botao.appendChild(imagem);
+          botao.appendChild(document.createTextNode(item.nome));
+          if (item.nome == vestido.manga) {
+            botao.classList.add('ativo');
+          }
+        });
+      } else if (index === 2 && item.classList.contains('ativo')) {
+        comprimentoReverso.forEach((item) => {
+          const botao = document.createElement('a');
+          const imagem = document.createElement('img');
+          const primeiroFilho = botoes.firstChild;
+          botoes.insertBefore(botao, primeiroFilho);
+          imagem.src = item.imagem;
+          botao.appendChild(imagem);
+          botao.appendChild(document.createTextNode(item.nome));
+          if (item.nome == vestido.comprimento) {
+            botao.classList.add('ativo');
+          }
+        });
+      } else if (index === 3 && item.classList.contains('ativo')) {
+        tamanhoReverso.forEach((item) => {
+          const botao = document.createElement('a');
+          botao.style.fontSize = 18 + 'px';
+          const tamanho = document.createElement('p');
+          const primeiroFilho = botoes.firstChild;
+          botoes.insertBefore(botao, primeiroFilho);
+
+          botao.appendChild(document.createTextNode(item));
+
+          if (item == vestido.tamanho) {
+            botao.classList.add('ativo');
+          }
+        });
       }
     });
-  });
+    render();
+  }
+  function render() {
+    customizar.querySelectorAll('a').forEach((item) => {
+      if (item.classList.contains('selecionado'))
+        item.classList.remove('ativo');
+    });
+    customizar.querySelectorAll('a').forEach((item, index) => {
+      item.addEventListener('click', () => {
+        if (
+          item.classList.contains('selecionavel') ||
+          item.classList.contains('selecionado')
+        ) {
+          item.classList.add('ativo');
+          item.classList.remove('selecionavel');
+
+          if (index != 0) {
+            customizar
+              .querySelectorAll('a')
+              [index - 1].classList.remove('ativo');
+            customizar
+              .querySelectorAll('a')
+              [index - 1].classList.add('selecionado');
+          }
+
+          verificarAtivo();
+        }
+      });
+    });
+
+    botoes.querySelectorAll('a').forEach((item) => {
+      item.addEventListener('click', () => {
+        botoes.querySelectorAll('a').forEach((item) => {
+          item.classList.remove('ativo');
+        });
+        item.classList.add('ativo');
+        // liberar botoes para trocar de customização
+        customizar.querySelectorAll('a').forEach((custom, index) => {
+          if (custom.classList.contains('ativo')) {
+            if (index == 0) {
+              vestido.decote = item.childNodes[1].nodeValue;
+              console.log(vestido);
+            } else if (index == 1) {
+              vestido.manga = item.childNodes[1].nodeValue;
+              console.log(vestido);
+            } else if (index == 2) {
+              vestido.comprimento = item.childNodes[1].nodeValue;
+              console.log(vestido);
+            } else if (index == 3) {
+              vestido.tamanho = item.childNodes[0].nodeValue;
+              console.log(vestido);
+            }
+            customizar.querySelectorAll('a')[index].classList.remove('ativo');
+            customizar
+              .querySelectorAll('a')
+              [index].classList.add('selecionado');
+            if (index != 3) {
+              customizar
+                .querySelectorAll('a')
+                [index + 1].classList.add('selecionavel');
+            }
+          }
+        });
+      });
+    });
+  }
+  verificarAtivo();
+  render();
 }
--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,117 +1,107 @@
 export default function cards() {
-    const pessoas = document.querySelector('.pessoas');
-    
-    const clientes = [
-        {
-            foto:'img/image 3.png',
-            estrelas: 4,
-            nome: 'Raine Gonçalves',
-            compra:{
-                decote:'Alta',
-                botoesManga:'Longa',
-                botoesComprimento:'Curto',
-                tamanho:'P'
+  const pessoas = document.querySelector('.pessoas');
 
-            },
-            descricao:'“Eu estou simplesmente apaixonada pelo meu vestido preto, que de básico, não tem nada! Conheci a loja através do instagram e me apaixonei pela ideia de ter um vestido preto que se adequa a diversas ocasiões. O produto chegou na minha casa dentro do prazo, fiquei encantada com o cuidado com que o vestido é embalado e adorei saber que eles se preocupam com o meio ambiente.O provador virtual me ajudou a escolher o tamanho perfeito.”'
-        },
-        {
-            foto:'img/image 4.png',
-            estrelas: 5,
-            nome: 'Ismarina Fernandes',
-            compra:{
-                decote:'princesa',
-                botoesManga:'Curta',
-                botoesComprimento:'longo',
-                tamanho:'M',
+  const clientes = [
+    {
+      foto: 'img/image 3.png',
+      estrelas: 4,
+      nome: 'Raine Gonçalves',
+      compra: {
+        decote: 'Alta',
+        botoesManga: 'Longa',
+        botoesComprimento: 'Curto',
+        tamanho: 'P',
+      },
+      descricao:
+        '“Eu estou simplesmente apaixonada pelo meu vestido preto, que de básico, não tem nada! Conheci a loja através do instagram e me apaixonei pela ideia de ter um vestido preto que se adequa a diversas ocasiões. O produto chegou na minha casa dentro do prazo, fiquei encantada com o cuidado com que o vestido é embalado e adorei saber que eles se preocupam com o meio ambiente.O provador virtual me ajudou a escolher o tamanho perfeito.”',
+    },
+    {
+      foto: 'img/image 4.png',
+      estrelas: 5,
+      nome: 'Ismarina Fernandes',
+      compra: {
+        decote: 'princesa',
+        botoesManga: 'Curta',
+        botoesComprimento: 'longo',
+        tamanho: 'M',
+      },
+      descricao: '“Gostei muito do meu vestido preto. “',
+    },
+    {
+      foto: 'img/image 5.png',
+      estrelas: 4,
+      nome: 'Thamyres',
+      compra: {
+        decote: 'Alta',
+        botoesManga: 'Longa',
+        botoesComprimento: 'Curto',
+        tamanho: 'G',
+      },
+      descricao:
+        '“O vestido é muito maravilhoso, eu tive problemas ao escolher o tamanho mas a loja foi muito atenciosa e o processo de troca foi descomplicado. Agora estou com o tamanho certo e o caimento ficou perfeito.Com certeza indico a Vestido preto e voltarei a fazer compras!”',
+    },
+  ];
 
-            },
-            descricao:'“Gostei muito do meu vestido preto. “'
-        },
-        {
-            foto:'img/image 5.png',
-            estrelas: 4,
-            nome: 'Thamyres',
-            compra:{
-                decote:'Alta',
-                botoesManga:'Longa',
-                botoesComprimento:'Curto',
-                tamanho: 'G',
-
-            },
-            descricao:'“O vestido é muito maravilhoso, eu tive problemas ao escolher o tamanho mas a loja foi muito atenciosa e o processo de troca foi descomplicado. Agora estou com o tamanho certo e o caimento ficou perfeito.Com certeza indico a Vestido preto e voltarei a fazer compras!”'
-        },
-
-    ]
-
-clientes.forEach(item=>{
-    
+  clientes.forEach((item) => {
     const card = document.createElement('div');
     card.classList.add('card');
-    
+
     const perfil = document.createElement('div');
     perfil.classList.add('perfil');
-    
+
     const imagem = document.createElement('img');
     imagem.src = item.foto;
-    
+
     const informacoes = document.createElement('div');
     informacoes.classList.add('informacoes');
-    
+
     const estrelas = document.createElement('div');
     estrelas.classList.add('estrelas');
-    
-    for(var i=0; i<=4; i++){
-        if(i<item.estrelas){
-            const estrelaCheia = document.createElement('img');
-            estrelaCheia.src = "img/star (1).svg";
-            estrelas.appendChild(estrelaCheia);
-            
-        }else{
-            const estrelaVazia = document.createElement('img');
-            estrelaVazia.src = "img/star.svg";
-            estrelas.appendChild(estrelaVazia)
-        }
+
+    for (var i = 0; i <= 4; i++) {
+      if (i < item.estrelas) {
+        const estrelaCheia = document.createElement('img');
+        estrelaCheia.src = 'img/star (1).svg';
+        estrelas.appendChild(estrelaCheia);
+      } else {
+        const estrelaVazia = document.createElement('img');
+        estrelaVazia.src = 'img/star.svg';
+        estrelas.appendChild(estrelaVazia);
+      }
     }
-    
-    const nome = document.createElement('h3')
+
+    const nome = document.createElement('h3');
     nome.appendChild(document.createTextNode(item.nome));
-    
+
     const descricao = document.createElement('div');
     descricao.classList.add('descricao');
     descricao.appendChild(document.createTextNode(item.descricao));
-    
+
     const span = document.createElement('span');
-    console.log(item.compra.decote)
-    span.appendChild(document.createTextNode('Vestido Preto One: Gola '+ 
-    item.compra.decote 
-    +', Manga '+
-    item.compra.botoesManga
-    +', '
-    +item.compra.botoesComprimento
-    +', tamanho '
-    +item.compra.tamanho));
-    
+
+    span.appendChild(
+      document.createTextNode(
+        'Vestido Preto One: Gola ' +
+          item.compra.decote +
+          ', Manga ' +
+          item.compra.botoesManga +
+          ', ' +
+          item.compra.botoesComprimento +
+          ', tamanho ' +
+          item.compra.tamanho
+      )
+    );
 
     informacoes.appendChild(estrelas);
     informacoes.appendChild(nome);
     informacoes.appendChild(span);
-    
+
     perfil.appendChild(imagem);
     perfil.appendChild(informacoes);
-    
+
     card.appendChild(perfil);
     card.appendChild(descricao);
-    
-    
-
-
-    
 
     pessoas.appendChild(card);
-})
-
-    
-    
-    
-  }
+  });
+}
